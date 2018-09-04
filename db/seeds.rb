291f3d8d1f28a3672d5a3efdcf4f48a729a1e17f
Brian Pruitt-Goddard <pruittgoddard.b@gmail.com>
2018-09-03 22:25:42 -0700

    feat(rails): Add unknown player to database seed
    Ensure there is always the unknown player

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+unknownPlayer = Player.find_or_create_by(id: -1)
+unknownPlayer.name = 'Unknown'
+unknownPlayer.save!
